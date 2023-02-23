90a429577a05a340562ff3768c74605726865f6c
Gino <35615@ma-web.nl>
2023-02-23 22:16:35 +0100

    extra is af

--- CSharpOpdrachten/Program.cs
+++ CSharpOpdrachten/Program.cs
@@ -4,8 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, string World!");
-            Console.ReadLine();
+            //maak een string
+            string text = "Hello, Wordl!";
+
+            //Hiermee kan je de kleur van de letters veranderen.
+            Console.ForegroundColor
+            = ConsoleColor.Green;
+
+            //om het woord te laten verschijnen moeten we een foreach loop maken
+            foreach (char letter in text)
+            {
+                Console.Write(letter);
+                Thread.Sleep(75);
+                //de Thread zorgt ervoor hoe snel het woord word uitgetyped.
+                //Hoe hoger het getal hoe langer hij er over doet en hoe kleiner het getal hoe sneller die het uit typed 
+            }
+            //Readkey zorgt ervoor dat die de loop gebruikt
+            Console.ReadKey();
+
+
         }
     }
 }
